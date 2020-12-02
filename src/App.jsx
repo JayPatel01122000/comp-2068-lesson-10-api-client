05a05442ea6a861aeae28ef954671fda570c6b46
Shaun McKinnon <shaun.mckinnon@georgiancollege.ca>
2020-12-02 15:26:04 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserProvider from './Authentication/UserProvider';
+
+const App = () => {
+  return (
+    <UserProvider>
+      
+    </UserProvider>
+  );
+};
+
+export default App;
